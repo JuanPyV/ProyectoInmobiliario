2a71ee9a7f22f300f323b2d42aa7b94ffc204dd5
Mayra Guajardo <a01634475@itesm.mx>
2019-04-25 09:02:25 -0500

    no se que sea

--- Inmobiliaria/src/LecturaCSV.java
+++ Inmobiliaria/src/LecturaCSV.java
@@ -3,7 +3,29 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class LecturaCSV {
+public class LecturaCSV<E> {
+	
+	
+	
+	
+	public String referencia(String ref) { //ref es un número dado por el cliente
+		String filename = "docs/Inmuebles.csv";
+		File file = new File (filename);
+		try {
+			Scanner inputStream = new Scanner(file);
+			while(inputStream.hasNext()) {
+				String data = inputStream.next();
+				String[] values = data.split(",");
+				for (int i = 0; i)
+				System.out.println(values[8] + "**");
+			}
+			inputStream.close();
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
