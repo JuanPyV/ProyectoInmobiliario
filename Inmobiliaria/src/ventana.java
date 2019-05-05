efca6c9cf59aa6e66cc185a6c571254934bcaac4
JuanPy <juanpa.jpvv@gmail.com>
2019-05-05 17:27:26 -0500

    Agregar imagen

--- Inmobiliaria/src/ventana.java
+++ Inmobiliaria/src/ventana.java
@@ -3,12 +3,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -43,15 +45,13 @@ public class ventana {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frameV = new JFrame();
-		frameV.setBounds(100, 100, 640, 490);
+		frameV.setBounds(100, 100, 650, 490);
 		frameV.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JLabel lblNewLabel = new JLabel("Inmobiliaria");
 		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 24));
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.RED);
 		
 		JButton btnNewButton = new JButton("Buscar");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -72,37 +72,47 @@ public class ventana {
 		JButton btnNewButton_2 = new JButton("Ver Datos");
 		
 		JButton btnNewButton_3 = new JButton("Borrar");
+		
+		
+		ImageIcon imagen = new ImageIcon("src/inmobiliaria.png");	
+		Image image = imagen.getImage();
+		Image newimg = image.getScaledInstance(620, 250,  java.awt.Image.SCALE_SMOOTH);		
+		ImageIcon imagencita = new ImageIcon(newimg);
+		JLabel lblNewLabel_1 = new JLabel();
+		lblNewLabel_1.setIcon(imagencita);
+		
 		GroupLayout groupLayout = new GroupLayout(frameV.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(54)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(btnNewButton)
-									.addGap(77)
-									.addComponent(btnNewButton_1)
-									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(btnNewButton_2)
-									.addGap(60)
-									.addComponent(btnNewButton_3)
-									.addGap(8))
-								.addComponent(panel, GroupLayout.PREFERRED_SIZE, 523, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(groupLayout.createSequentialGroup()
 							.addGap(227)
-							.addComponent(lblNewLabel)))
-					.addContainerGap(47, Short.MAX_VALUE))
+							.addComponent(lblNewLabel))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addGap(54)
+							.addComponent(btnNewButton)
+							.addGap(77)
+							.addComponent(btnNewButton_1)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnNewButton_2)
+							.addGap(60)
+							.addComponent(btnNewButton_3)
+							.addGap(8)))
+					.addGap(47))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGap(33)
+					.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
+					.addGap(28))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(22)
 					.addComponent(lblNewLabel)
-					.addGap(35)
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 176, GroupLayout.PREFERRED_SIZE)
-					.addGap(29)
+					.addGap(31)
+					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+					.addGap(59)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnNewButton)
 						.addComponent(btnNewButton_1)
